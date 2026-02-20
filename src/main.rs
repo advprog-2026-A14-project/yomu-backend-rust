4f2ee89b9a074bf743128ab2159d1745b2529955
farrelzzz <rajendramuhammadfarrel@gmail.com>
2026-02-20 20:38:46 +0700

    add modules routes and adjusted  database_url in src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use std::net::SocketAddr;
 use tower_http::cors::{Any, CorsLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
+use sqlx::postgres::PgPoolOptions;
+use std::env;
+
+mod modules;
+
 #[derive(Serialize, Deserialize)]
 struct HealthResponse {
     status: String,
@@ -21,7 +26,9 @@ async fn health_check() -> (StatusCode, Json<HealthResponse>) {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), sqlx::Error> {
+    dotenvy::dotenv().ok();
+
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -32,6 +39,14 @@ async fn main() {
 
     tracing::info!("Starting Yomu Engine Rust...");
 
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL tidak ditemukan di .env");
+    tracing::info!("Connecting to database...");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
+    tracing::info!("Succesfully connected to database");
+    
     let cors = CorsLayer::new()
         .allow_origin(Any)
         .allow_methods(Any)
@@ -39,11 +54,15 @@ async fn main() {
 
     let app = Router::new()
         .route("/health", get(health_check))
-        .layer(cors);
+        .merge(modules::all_routes())
+        .layer(cors)
+        .with_state(pool);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     tracing::info!("Listening on {}", addr);
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
+
+    Ok(())
 }
