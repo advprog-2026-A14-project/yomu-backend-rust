c9f67230372f59117fe51d2e90966cf20666904a
farrelzzz <rajendramuhammadfarrel@gmail.com>
2026-02-20 20:37:14 +0700

    setup routes for modules

--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -1,1 +1,10 @@
 // Modules (Bounded Contexts) - League, Gamification, User Sync
+use axum::Router;
+use sqlx::PgPool;
+
+pub mod gamification;
+
+pub fn all_routes() -> Router<PgPool> {
+    Router::new()
+        .merge(gamification::routes())
+}
