884313d1da2cb5373e4d29b4ec53be909f138a19
HeraldoArman <heraldoarman7@gmail.com>
2026-05-21 14:13:08 +0700

    feat(league): add ClanJoinRequestRepository trait and Postgres implementation

--- src/modules/league/domain/repositories/mod.rs
+++ src/modules/league/domain/repositories/mod.rs
@@ -1,9 +1,11 @@
 pub mod clan_buff_repository;
+pub mod clan_join_request_repository;
 pub mod clan_repository;
 pub mod leaderboard_cache;
 pub mod season_repository;
 
 pub use clan_buff_repository::ClanBuffRepository;
+pub use clan_join_request_repository::ClanJoinRequestRepository;
 pub use clan_repository::ClanRepository;
 pub use leaderboard_cache::LeaderboardCache;
 pub use season_repository::SeasonRepository;
